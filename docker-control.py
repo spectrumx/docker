eb83471922c13129ad5ad2563e5ec1dde5ee792c
Randy Herban <rherban@nd.edu>
2025-03-18 12:37:24 -0400

    Separate out docker the module from the variable so we can check for errors properly.

--- docker-control.py
+++ docker-control.py
@@ -3,7 +3,7 @@ import docker
 import time
 import json
 
-docker = docker.from_env()
+docker_client = docker_client.from_env()
 
 service_name = "docker"
 announce_packet = {
@@ -45,10 +45,9 @@ def on_message(client, userdata, msg):
 
 def send_status(client):
   global service_name
-  payload = {"state": "online", "timestamp": time.time()}
   # Get Docker container status
   containers_info = []
-  for container in docker.containers.list():
+  for container in docker_client.containers.list():
     container_info = {
         'id': container.id,
         'name': container.name,
@@ -56,20 +55,28 @@ def send_status(client):
         'image': container.image.tags
     }
     containers_info.append(container_info)
-  payload["docker"] = containers_info
+  #payload["docker"] = containers_info
+  payload = {
+      "state": "online", 
+      "timestamp": time.time(),
+      "task_name": "tasks.admin.save_docker",
+      "arguments": containers_info,
+      }
   client.publish(service_name + "/status", json.dumps(payload), retain=True)
 
 def start_container(container_name):
     try:
-        container = docker.containers.get(container_name)
+        container = docker_client.containers.get(container_name)
         container.start()
         print(f"Started container: {container_name}")
     except docker.errors.NotFound:
         print(f"Container not found: {container_name}")
+    except docker.APIError as e:
+        print(f"API error: {e}")
 
 def stop_container(container_name):
     try:
-        container = docker.containers.get(container_name)
+        container = docker_client.containers.get(container_name)
         container.stop()
         print(f"Stopped container: {container_name}")
     except docker.errors.NotFound:
@@ -77,7 +84,7 @@ def stop_container(container_name):
 
 def pull_container(image_name):
     try:
-        docker.images.pull(image_name)
+        docker_client.images.pull(image_name)
         print(f"Pulled image: {image_name}")
     except docker.errors.APIError as e:
         print(f"Failed to pull image: {image_name}, error: {e}")
